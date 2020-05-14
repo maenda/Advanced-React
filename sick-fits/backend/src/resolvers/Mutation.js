3b059c719bd60ac85ad62917253359a272288f43
Maenda Fortier <maenda@pixelunion.net>
2020-05-14 06:12:33 -0400

    Step 14

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(parents, args, ctx, info) {
+    // todo: check if they're logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info)
+
+    return item;
+  }
+};
 
 module.exports = Mutation;
