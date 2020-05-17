e082854324c0e06e84f077c12f48894020137973
Maenda Fortier <maenda@pixelunion.net>
2020-05-17 07:48:40 -0400

    After video 18

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -30,8 +30,8 @@ class CreateItem extends Component {
   state = {
     title: 'cool',
     description: 'desc',
-    image: 'dog.jog',
-    largeImage: 'large-dog.jpg',
+    image: '',
+    largeImage: '',
     price: 1,
   };
 
@@ -39,6 +39,26 @@ class CreateItem extends Component {
     const { name, type, value } = e.target;
     const val = type === 'number' ? parseFloat(value) : value;
     this.setState({ [name]: val })
+  }
+
+  uploadFile = async (e) => {
+    console.log('uploading file...');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/diz9vl2qx/image/upload', {
+        method: 'POST',
+        body: data
+      });
+    const file = await res.json();
+
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
   }
 
   render() {
@@ -59,6 +79,19 @@ class CreateItem extends Component {
           }}>
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+            <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an Image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && <img src={this.state.image} alt="Upload Preview " />}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input
